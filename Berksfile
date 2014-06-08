5270238013ddfc6dac6afa9f43337726963111ee
Patrick Byrne <code@patrickbyrne.net>
2014-06-07 20:49:30 -0500

    Install nodejs

--- Berksfile
+++ Berksfile
@@ -3,6 +3,7 @@ source "https://api.berkshelf.com"
 cookbook "logrotate"
 cookbook "main", path: "./site-cookbooks/main"
 cookbook "nginx"
+cookbook "nodejs"
 cookbook "sites", path: "./site-cookbooks/sites"
 
 
--- roles/web.rb
+++ roles/web.rb
@@ -4,9 +4,15 @@ default_attributes({
   nginx: {
     gzip_comp_level: 9,
   },
+  nodejs: {
+    install_method: "binary",
+    version: "0.10.28",
+    checksum_linux_x64: "5f41f4a90861bddaea92addc5dfba5357de40962031c2281b1683277a0f75932",
+  },
 })
 
 run_list([
   "recipe[nginx]",
+  "recipe[nodejs]",
   "recipe[sites]",
 ])
